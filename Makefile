3a4ea4393c039ae0eb49bea996282e9d82296595
Xing Chen <xingchen@f2r10s12.clusters.42paris.fr>
2026-02-25 17:04:48 +0100

    add

--- Makefile
+++ Makefile
@@ -6,13 +6,44 @@
 #    By: xingchen <xingchen@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/02/19 16:00:15 by xingchen          #+#    #+#              #
-#    Updated: 2026/02/19 16:00:23 by xingchen         ###   ########.fr        #
+#    Updated: 2026/02/25 13:26:50 by xingchen         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 CC = cc
-CFLAGS = -Wall -Werror -Wextra
+CFLAGS = -Wall -Werror -Wextra -Ilibft -Ift_print
 NAME = push_swap
+
+SRC = main.c
+OBJS = $(SRC:.c=.o)
 #LIBFT_DIR = libft
+LIBFT_DIR = libft
+FT_PRINTF_DIR = ft_printf
 LIBFT = libft/libft.a
 FT_PRINTF = ft_printf/libftprintf.a
+#当执行make的时候 默认执行all all；依赖NAME
+all: $(NAME)
+#要生成NAME 要先生成OBJS
+$(NAME): $(OBJS)
+#生成NAME的方式 -C就是CD 进入到目录 MAKE是Makefile的内置变量指代当前使用的make命令
+	$(MAKE) -C $(LIBFT_DIR)
+	$(MAKE) -C $(FT_PRINTF_DIR)
+	$(CC) $(CFLAGS) $(OBJS) $(LIBFT) $(FT_PRINTF) -o $(NAME)
+#任何.o文件都可以用由同名文件.c编译得到
+#（$<）第一个依赖文件（.c）   （$@）目标文件（.o）
+%o: %c
+	$(CC) $(CFLAG) -c $< -o $@
+clean:
+#进入到自己的文件夹执行自己的clean
+	$(MAKE) -C $(LIBFT_DIR) clean
+	$(MAKE) -C $(FT_PRINTF_DIR) clean
+	rm -f $(OBJS)
+fclean: clean
+	$(MAKE) -C $(LIBFT_DIR) fclean
+	$(MAKE) -C $(FT_PRINTF_DIR) fclean
+	rm -f $(NAME)
+	
+re: fclean all
+.PHONY: all clean fclean re
+
+	
--- main.c
+++ main.c
@@ -6,26 +6,179 @@
 /*   By: xingchen <xingchen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 12:23:17 by xingchen          #+#    #+#             */
-/*   Updated: 2026/02/20 14:11:00 by xingchen         ###   ########.fr       */
+/*   Updated: 2026/02/25 13:42:00 by xingchen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "libft.h"
 
+long	ft_atol(const char *nptr)
+{
+	long	i;
+	long	result;
+	long	sign;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+	while (nptr[i] == 32 || (9 <= nptr[i] && nptr[i] <= 13))
+		i ++;
+	if (nptr[i] == '+' || nptr[i] == '-')
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i ++;
+	}
+	while ('0' <= nptr[i] && nptr[i] <= '9')
+	{
+		result = result * 10 + (nptr[i] - '0');
+		i ++;
+	}
+	return (result * sign);
+}
+char	*ft_join(char *s1, char *s2, char c)
+{
+	size_t	s2_len;
+	size_t	s1_len;
+	char	*dup;
+
+	if (!s1)
+		s1_len = 0;
+	else
+		s1_len = ft_strlen(s1);
+	if (!s2)
+		s2_len = 0;
+	else
+		s2_len = ft_strlen(s2);
+	dup = malloc(s1_len + s2_len + 2);
+	if (!dup)
+		return (NULL);
+	ft_memcpy(dup, s1, s1_len);
+	dup[s1_len] = c;
+	ft_memcpy(dup + s1_len + 1, s2, s2_len);
+	dup[s1_len + s2_len + 1] = '\0';
+	free(s1);
+	return (dup);
+}
 int	main(int argc, char ** argv)
 {
 	int	i;
-	int i;
-	if (argc == 1 && (argc == 2 && !argv[1]))
+	int	j;
+	long a;
+	long b;
+	char *join = NULL;;
+	char ** split = {NULL};
+	
+	if (argc == 1 || (argc == 2 && !argv[1]))
 		return (0);
 	if(argc == 2 && argv[1][0] == '\0')
 		return (write(2,"Error\n",6));
+	
+
+	//组合所有的元素、重新拆分
+	i = 1;
+	
+	while (i < argc)
+	{
+		join = ft_join(join, argv[i], ' ');
+		i ++;
+	}
+	
+	split = ft_split(join, 32);
 	i = 0;
-	char **dup = ft_split(&argv[1], 32);
+	while(split[i])
+	{
+		printf("%s\n",split[i]);
+		i ++;
+	}
+	
+//判断是不是数字来输出error
+	i = 0;
+	while(split[i])
+	{
+		j = 0;
+		while (split[i][j])
+		{
+			if(split[i][j] == '+' || split[i][j] == '-')
+				j ++;
+			if(!ft_isdigit(split[i][j]))
+				return(write(2,"Error\n",6));
+			while(ft_isdigit(split[i][j]))
+				j ++;
+			if(split[i][j] && !ft_isdigit(split[i][j]))
+				return(write(2,"Error\n",6));	
+		}
+		i ++;
+	}
+	//判断有没有重复数字
+	i = 0;
+	while(split[i])
+	{
+		j = 0;
+		a = ft_atol(split[i]);
+		if(-2147483648 > a || a > 2147483647)
+			return(write(2,"Error\n",6));
+		while (split[j])
+		{
+			b = ft_atoi(split[j]);
+			if(-2147483648 > b || b > 2147483647)
+				return(write(2,"Error\n",6));
+			if (a == b && i!=j)
+				return(write(2,"Error\n",6));
+			j ++;
+		}
+		i ++;
+		
+	}
+	//判断INT是不是超MAX
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		/*if(split[i][0] == '0')
+			i ++;
+		else
+		{
+			j = ft_atoi(split[i]);
+			printf("%d\n", j);
+			i ++;
+			if (j == 0)
+				return (write(2,"Error\n",6));
+		}*/
+	
+	i = 0;
+	while(split[i])
+	{
+		free(split[i]);
+		i ++;
+	}
+	free(split);
+	free(join);
+}
+
+
+	
+	/*
+	//判断array or string
+while (argv[1][i] == 32 && argv[1][i])
+	i ++;
+while(argv[1][i] != 32 && argv[1][i])
+	i ++;
+while (argv[1][i] == 32 && argv[1][i])
+	i ++;
+if (argv[1][i] != 32 && argv[1][i])
+{
+	dup = ft_split(argv[1], 32);
+	i = 0;
 	while(dup[i])
 	{
-		printf("%s",dup[i]);
+		printf("%s\n",dup[i]);
 		i ++;
 	}
 	i = 0;
@@ -35,10 +188,36 @@ int	main(int argc, char ** argv)
 		i ++;
 	}
 	free(dup);
+}
+	//判断是否是数字
+	i = ft_atoi(argv[1]);
+	printf("%d", i);*/
+	/*//解析成字符串组
+	char **dup = ft_split(argv[1], 32);
+	//测试是否有不可能转化成数字的字符串
+
+
+
+
+
+
+	
+	while(dup[i])
+	{
+		printf("%s\n",dup[i]);
+		i ++;
+	}
+	i = 0;
+	while(dup[i])
+	{
+		free(dup[i]);
+		i ++;
+	}
+	free(dup);*/
 	/*while (argv[1][i])
 	{
 		
 	}
 		return (write(1,"yes!\n",5));*/
 		
-}
+
