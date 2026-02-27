0072b7047c565f67b9cfc5f08f6aeaa8d74430c1
xingchen <xingchen@student.42.fr>
2026-02-27 22:35:40 +0100

    dd

--- push_sa.c
+++ push_sa.c
@@ -6,7 +6,7 @@
 /*   By: xingchen <xingchen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 22:26:33 by xingchen          #+#    #+#             */
-/*   Updated: 2026/02/26 23:49:39 by xingchen         ###   ########.fr       */
+/*   Updated: 2026/02/27 14:53:09 by xingchen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,19 @@ t_stack *creat_end_node(t_stack *head)
 int push_sa(t_stack **stack_a,t_stack **stack_b)
 {
     t_stack *temp_a;
+    
     temp_a = *stack_a;
-    *stack_a = (*stack_a)->next;
+    (*stack_a) = (*stack_a)->next;
+   
+    
     if (!*stack_b)
-    {
-        *stack_b = temp_a;
-        temp_a->next = NULL;
+    {   *stack_b = temp_a;
+        
+        (*stack_b)->next = NULL;
+        printf("kkaa:%d\n",(*stack_b)->value);
     }
     else
-    {    
+    {   
         temp_a->next = (*stack_b)->next;
         *stack_b = temp_a;
     }
