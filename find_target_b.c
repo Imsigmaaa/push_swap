2466db649b0971a4eaed0b41c915a0f077ce52a0
Chen Xing <xing@DESKTOP-H0KNCUR.localdomain>
2026-03-07 05:58:06 +0100

    add

--- find_target_b.c
+++ find_target_b.c
@@ -6,61 +6,30 @@
 /*   By: xingchen <xingchen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/04 18:56:36 by xingchen          #+#    #+#             */
-/*   Updated: 2026/03/07 04:01:20 by xingchen         ###   ########.fr       */
+/*   Updated: 2026/03/07 05:57:49 by xingchen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack	*find_biggest_value(t_stack **stack)
+void	find_best_target(int index, t_stack **big, t_stack **least, t_stack **bigltl)
 {
-	t_stack	*big;
 	t_stack	*temp;
 
-	temp = *stack;
-	big = *stack;
+	temp = *big;
 	while (temp)
 	{
-		if (temp->index > big->index)
-			big = temp;
-		temp = temp->next;
-	}
-	return (big);
-}
-
-t_stack	*find_least_value(t_stack **stack)
-{
-	t_stack	*least;
-	t_stack	*temp;
-
-	temp = *stack;
-	least = *stack;
-	while (temp)
-	{
-		if (temp->index < least->index)
-			least = temp;
-		temp = temp->next;
-	}
-	return (least);
-}
-
-t_stack	*find_largest_less_than(int index, t_stack **stack)
-{
-	t_stack	*biglittle;
-	t_stack	*temp;
-
-	temp = *stack;
-	biglittle = NULL;
-	while (temp)
-	{
+		if (temp->index > (*big)->index)
+			*big = temp;
+		if (temp->index < (*least)->index)
+			*least = temp;
 		if(temp->index < index)
 		{
-			if (!biglittle || temp->index > biglittle->index)
-				biglittle = temp;
+			if (!*bigltl || temp->index > (*bigltl)->index)
+				*bigltl = temp;
 		}
 		temp = temp->next;
 	}
-	return (biglittle);
 }
 
 t_stack	*find_target_b(int index, t_stack **stack_b)
@@ -70,14 +39,13 @@ t_stack	*find_target_b(int index, t_stack **stack_b)
 	t_stack	*least;
 	t_stack	*target_b;
 
-	big = find_biggest_value(stack_b);
-	least = find_least_value(stack_b);
+	big = *stack_b;
+	least = *stack_b;
+	ltlbig = NULL;
+	find_best_target(index,&big,&least,&ltlbig);
 	if (index > big->index || index < least->index)
 		target_b = big;
 	else
-	{
-		ltlbig = find_largest_less_than(index, stack_b);
 		target_b = ltlbig;
-	}
 	return (target_b);
 }
